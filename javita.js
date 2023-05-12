74caed3b1b85edad5860dcfe8cac6ef016b684e0
Linderman343 <leonelpala234@gmail.com>
2023-05-12 02:43:56 -0300

    terminado

--- javita.js
+++ javita.js
@@ -4,9 +4,10 @@ let random = Math.floor(Math.random()* (129 - 0) + 0)
 numero.textContent = 'Calcula el numero ' + random 
 alert((random).toString(2))
 
-for (let i = 0; i < 8; i++) {
-document.getElementById('table').innerHTML += `<input class="casillas" id="btn_${i}"type="button" value="0" onclick="llamado(btn_${i}.id)">`
-}
+
+    for (let i = 0; i < 8; i++) {
+        document.getElementById('table').innerHTML += `<input class="casillas" id="btn_${i}"type="button" value="0" onclick="llamado(btn_${i}.id)">`
+        }    
 
 
 //cambiar 0 y 1
@@ -24,22 +25,23 @@ let binario_todo = ""
 
 function todo(a) {
     binario_todo = ""
-    document.getElementById(`resultados`).innerHTML += `<div class="reference"><p>${random}:</p></div>`
+    document.getElementById(`resultados`).innerHTML += `<div class="btn_res" id="rand_res"><p>${random}</p></div>`
     for (let i = 0; i < 8; i++) {
 
     let btn = document.querySelector(`#btn_${i}`)
-    document.getElementById(`resultados`).innerHTML += `<div class="reference"><p>${btn.value}</p></div>`
+    document.getElementById(`resultados`).innerHTML += `<div class="btn_res bin_res"><p>${btn.value}</p></div>`
     binario_todo += String(btn.value)
+    btn.value = 0
     }
 
     let binario_decimal = parseInt(binario_todo,2);
     if (binario_decimal == random ) {
-        document.getElementById(`resultados`).innerHTML += `<div id="correct">o</div>`
+        document.getElementById(`resultados`).innerHTML += `<div class="btn_res"id="correct"><p>✔</p></div>`
     }
     else{
-        document.getElementById(`resultados`).innerHTML += `<div id="incorrect">X</div>`
+        document.getElementById(`resultados`).innerHTML += `<div class="btn_res" id="incorrect"><p>ᚷ</p></div>`
     }
     random = Math.floor(Math.random()* (129 - 0) + 0)
-numero.textContent = 'Numero es:' + random 
+numero.textContent = 'Calcula el numero ' + random 
 alert((random).toString(2))
 }
