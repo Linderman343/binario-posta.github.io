f798040bd026a283cd87c2858fb590d29456a558
Linderman343 <leonelpala234@gmail.com>
2023-05-12 02:08:16 -0300

    subir

--- javita.js
+++ javita.js
@@ -1,12 +1,11 @@
 
-let numero = document.querySelector('#hola')
+let numero = document.querySelector('#randomNumber')
 let random = Math.floor(Math.random()* (129 - 0) + 0)
-numero.textContent = 'Numero es:' + random 
+numero.textContent = 'Calcula el numero ' + random 
+alert((random).toString(2))
 
-function activar(e) {
-    for (let i = 0; i < 8; i++) {
-    document.getElementById('table').innerHTML += `<input class="casillas" id="btn_${i}"type="button" value="0" onclick="llamado(btn_${i}.id)">`
-    }
+for (let i = 0; i < 8; i++) {
+document.getElementById('table').innerHTML += `<input class="casillas" id="btn_${i}"type="button" value="0" onclick="llamado(btn_${i}.id)">`
 }
 
 
@@ -22,10 +21,25 @@ function llamado(e) {
     }
 }
 let binario_todo = ""
+
 function todo(a) {
     binario_todo = ""
+    document.getElementById(`resultados`).innerHTML += `<div class="reference"><p>${random}:</p></div>`
     for (let i = 0; i < 8; i++) {
-    document.getElementById(`btn_${i}`).innerHTML = 
+
+    let btn = document.querySelector(`#btn_${i}`)
+    document.getElementById(`resultados`).innerHTML += `<div class="reference"><p>${btn.value}</p></div>`
+    binario_todo += String(btn.value)
     }
-    alert(binario_todo)
+
+    let binario_decimal = parseInt(binario_todo,2);
+    if (binario_decimal == random ) {
+        document.getElementById(`resultados`).innerHTML += `<div id="correct">o</div>`
+    }
+    else{
+        document.getElementById(`resultados`).innerHTML += `<div id="incorrect">X</div>`
+    }
+    random = Math.floor(Math.random()* (129 - 0) + 0)
+numero.textContent = 'Numero es:' + random 
+alert((random).toString(2))
 }
